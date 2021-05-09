a7e453a821650db04d0ca2534b5b750f7199cc6a
Tobi <39420707+TobidieTopfpflanze@users.noreply.github.com>
2021-05-09 05:25:06 +0200

    Fix build and improve project setup

--- demo/example.ts
+++ demo/example.ts
@@ -1,10 +1,17 @@
-import Logger from "../src/Logger"
-import LogProfile from "../src/LogProfile";
-import LogLevel from "../src/Types/LogLevelEnum";
+import Logger from '../src/Logger';
+import LogProfile from '../src/LogProfile';
+import LogLevel from '../src/Types/LogLevelEnum';
 
 const logger = new Logger().enable({
-  loglevel: LogLevel.INFO,
-  logProfiles: [new LogProfile({name: "test", prefix: "test", logLevel: LogLevel.INFO })],
-})
+    loglevel: LogLevel.INFO,
+    logProfiles: [
+        new LogProfile({ name: 'test', prefix: 'test', logLevel: LogLevel.INFO })
+    ]
+});
 
-logger.log("test", "EIne test nachricht für den logger");
+logger.log('test', 'Eine test nachricht für den logger');
+
+logger.log('info', 'Eine test nachricht für den logger');
+logger.log('warn', 'Eine test nachricht für den logger');
+logger.log('error', 'Eine test nachricht für den logger');
+logger.log('debug', 'Eine test nachricht für den logger');
