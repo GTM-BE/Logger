c3c5fd6c7655f33463d0c0b949aae8f5ba203ac5
Tobi <tobias.baldeheli@gmail.com>
2021-12-04 04:58:57 +0100

    Expose colors enum

--- src/Index.ts
+++ src/Index.ts
@@ -4,6 +4,7 @@ import { ConsolePipeline } from './Pipelines/ConsolePipeline';
 import { FilePipeline } from './Pipelines/FilePipeline';
 import { LogProfile } from './Types/LogProfile';
 import { Placeholder } from './Types/Placeholder';
+import { Colors } from './Types/Colors';
 
-export { Logger, ConsolePipeline, FilePipeline, Pipeline, Placeholder };
+export { Logger, ConsolePipeline, FilePipeline, Pipeline, Placeholder, Colors };
 export type { LogProfile };
